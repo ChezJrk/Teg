f5b84a1bbf2f1a71a58d11fc71f1548eea378ff8
Jesse Michel <jmmichel@mit.edu>
2021-03-20 00:34:36 -0400

    More minor edits.

--- teg/passes/substitute.py
+++ teg/passes/substitute.py
@@ -20,9 +20,7 @@ def substitute(expr: ITeg, this_expr: ITeg, that_expr: ITeg) -> ITeg:
 
 
 def substitute_instance(expr: ITeg, this_expr: ITeg, that_expr: ITeg) -> ITeg:
-    """
-    Substitutes a specific instance of expr (ignores exprs that are symbolically equal to expr but not the same)
-    """
+    """Substitutes a specific instance of expr (ignores exprs that are symbolically equal to expr but not the same). """
     def inner_fn(e, ctx):
         if e is this_expr:
             return that_expr, ctx
